40d15cb15906a08c6335dca970c3c959d176fa2b
Robin Hahling <robin.hahling@gw-computing.net>
2016-10-09 19:25:20 +0200

    Implement 'parse' and 'compose'

---
+++ index.js
@@ -0,0 +1,155 @@
+"use strict";
+
+var iconv = require("iconv-lite");
+
+
+/*
+ * Decode "%xx hex" encoded strings into a Buffer.
+ */
+function pct_decode(urlencoded) {
+    /*
+     * see https://tools.ietf.org/html/rfc3986#section-2.3
+     * unreserved  = ALPHA / DIGIT / "-" / "." / "_" / "~"
+     */
+    var unreserved = /[A-Za-z0-9\-\._~]/;
+
+    /*
+     * see https://tools.ietf.org/html/rfc3986#section-2.1
+     *  pct-encoded = "%" HEXDIG HEXDIG
+     */
+    var pct_encoded = /%[A-Fa-f0-9]{2}/;
+
+    var correctly_encoded = new RegExp(
+        "^(?:" + unreserved.source + "|" + pct_encoded.source + ")*$"
+    );
+
+    if (!urlencoded.match(correctly_encoded))
+        throw new Error("malformed data");
+
+    var splitter = new RegExp(
+        "(" + pct_encoded.source + ")"
+    );
+
+    var buffers = urlencoded.split(splitter).map(function (part) {
+        if (part.match(pct_encoded)) {
+            return Buffer.from(/* remove leading `%' */  part.slice(1), "hex");
+        } else {
+            return Buffer.from(part, "ascii");
+        }
+    });
+
+    return Buffer.concat(buffers);
+};
+
+/*
+ * Encode str into a percent encoded string.
+ */
+function pct_encode(str, charset) {
+    if (!iconv.encodingExists(charset))
+        throw new Error("unsupported charset (" + charset + ")");
+    /*
+     * see https://tools.ietf.org/html/rfc3986#section-2.3
+     * unreserved  = ALPHA / DIGIT / "-" / "." / "_" / "~"
+     */
+    var unreserved = /[A-Za-z0-9\-\._~]/;
+
+    var pct_encoded = str.split("").reduce(function (str, char) {
+        if (char.match(unreserved)) {
+            return str + char;
+        } else {
+            return str + iconv.encode(char, charset).toString("hex").match(/[A-Fa-f0-9]{2}/).map(function (hex) {
+                return "%" + hex;
+            });
+        }
+    }, "");
+
+    return pct_encoded;
+};
+
+
+module.exports = {
+    /*
+     * See RFC 2397 section 3 for more details about the dataurl format.
+     *
+     *   dataurl    := "data:" [ mediatype ] [ ";base64" ] "," data
+     *   mediatype  := [ type "/" subtype ] *( ";" parameter )
+     *   data       := *urlchar
+     *   parameter  := attribute "=" value*
+     *
+     */
+    parse: function (dataurl, callback) {
+        // capture groups:
+        //   (1) [ mediatype ] [ ";base64" ]
+        //   (2) data
+        var dataurl_splitter = /^(?:data:){1}([^,]*)(?:,)(.+)$/;
+        var split = dataurl.match(dataurl_splitter);
+        if (null === split)
+            return callback(new Error("malformed dataurl"));
+
+        var [mediatype, data] = split.slice(1);
+
+        mediatype = mediatype.split(";");
+
+        // base64 is last element (if present) and is a special case
+        var base64 = mediatype[mediatype.length - 1] === "base64" && mediatype.pop();
+
+        // first element of mediatype is the MIME (if present)
+        var mime = mediatype.shift() || 'text/plain';
+
+        var parameters = mediatype.reduce(function (params, param) {
+            var [key, value] = param.split("=", 2);
+            if (!key || !value)
+                return params; // be "nice" and ignore invalid properties
+            params[key.toLowerCase()] = value;
+            return params;
+        }, {});
+
+        if (mime.toLowerCase().startsWith('text/') && !parameters['charset'])
+            parameters['charset'] = 'US-ASCII';
+
+        try {
+            return callback(null, {
+                mime: mime,
+                parameters: parameters,
+                data: base64 ? Buffer.from(data, 'base64') : pct_decode(data),
+            });
+        } catch (err) {
+            return callback(err);
+        }
+    },
+
+    compose: function (obj, options, callback) {
+        if ('undefined' === typeof callback) {
+            callback = options;
+            options = {}
+        }
+
+        var mime = obj.mime || "";
+        var parameters = obj.parameters || {};
+        var charset = parameters.charset || "US-ASCII";
+        var parametersString = Object.keys(parameters).reduce(function (params, key) {
+            return params + ";" + key + "=" + parameters[key];
+        }, "");
+        var mediatype = mime + parametersString;
+
+        if (!iconv.encodingExists(charset))
+            return callback(new Error("unsupported charset (" + charset + ")"));
+
+        if (!Buffer.isBuffer(obj.data))
+            return callback(new Error("unexpected type for obj.data (did you provide a Buffer?)"));
+
+        var data = "";
+        if (true === options.base64) {
+            mediatype += ";base64";
+            data = obj.data.toString("base64");
+        } else {
+            try {
+                data = pct_encode(iconv.decode(obj.data, charset), charset);
+            } catch (err) {
+                return callback(err);
+            }
+        }
+
+        return callback(null, "data:" + mediatype + "," + data);
+    },
+};
---
+++ test/index.js
@@ -0,0 +1,360 @@
+"use strict";
+/* jshint -W030 */
+/* global describe:true */
+/* global it:true */
+
+var chai   = require("chai");
+var expect = chai.expect;
+var iconv  = require('iconv-lite');
+
+var rfc2397 = require('../');
+
+describe("node-rfc2397", function () {
+    describe("parse", function () {
+        describe("when given RFC 2397 examples", function () {
+            it("should parse the 'brief note' example successfully", function (done) {
+                rfc2397.parse("data:,A%20brief%20note", function (err, obj) {
+                    if (err)
+                        return done(err);
+                    expect(obj).to.deep.equal({
+                        mime: "text/plain",
+                        parameters: {
+                            charset: "US-ASCII",
+                        },
+                        data: Buffer.from("A brief note", "ascii")
+                    });
+                    return done();
+                });
+            });
+            it("should parse the 'charset parameter' example successfully", function (done) {
+                // note: original RFC text ('%be%fg%be') has been changed for
+                // '%be%d3%be' as proposed in errata (ID: 2009) since 'g' is
+                // obviously not a hex digit
+                // see https://www.rfc-editor.org/errata_search.php?eid=2009
+                rfc2397.parse("data:text/plain;charset=iso-8859-7,%be%d3%be", function (err, obj) {
+                    if (err)
+                        return done(err);
+                    expect(obj).to.deep.equal({
+                        mime: "text/plain",
+                        parameters: {
+                            charset: "iso-8859-7",
+                        },
+                        data: Buffer.from("bed3be", "hex"),
+                    });
+                    return done();
+                });
+            });
+            it("should parse the 'Larry' example successfully", function (done) {
+                var data = "R0lGODdhMAAwAPAAAAAAAP///ywAAAAAMAAwAAAC8Iy" +
+                "Pqcvt3wCcDkiLc7C0qwyGHhSWpjQu5yqmCYsapyuvUUlvONmOZtfzgFz" +
+                    "ByTB10QgxOR0TqBQejhRNzOfkVJ+5YiUqrXF5Y5lKh/DeuNcP5yLWGsE" +
+                    "btLiOSpa/TPg7JpJHxyendzWTBfX0cxOnKPjgBzi4diinWGdkF8kjdfn" +
+                    "ycQZXZeYGejmJlZeGl9i2icVqaNVailT6F5iJ90m6mvuTS4OK05M0vDk" +
+                    "0Q4XUtwvKOzrcd3iq9uisF81M1OIcR7lEewwcLp7tuNNkM3uNna3F2JQ" +
+                    "Fo97Vriy/Xl4/f1cf5VWzXyym7PHhhx4dbgYKAAA7";
+                var larry = "data:image/gif;base64," + data;
+                rfc2397.parse(larry, function (err, obj) {
+                    if (err)
+                        return done(err);
+                    expect(obj).to.deep.equal({
+                        mime: 'image/gif',
+                        parameters: {},
+                        data: Buffer.from(data, "base64"),
+                    });
+                    return done();
+                });
+            });
+            it("should parse the 'application/vnd-xxx-query' example successfully", function (done) {
+                // note: original RFC text (select_vcount,fcol_from_fieldtable/local)
+                // has been changed because it is not correctly percent encoded
+                var data = "select_vcount%2cfcol_from_fieldtable%2flocal";
+                var vnd = "data:application/vnd-xxx-query," + data;
+                rfc2397.parse(vnd, function (err, obj) {
+                    if (err)
+                        return done(err);
+                    expect(obj).to.deep.equal({
+                        mime: 'application/vnd-xxx-query',
+                        parameters: {},
+                        data: Buffer.from("select_vcount,fcol_from_fieldtable/local", "ascii"),
+                    });
+                    return done();
+                });
+            });
+        });
+        describe("when given a dataurl with several parameters", function () {
+            it("should parse it successfully", function (done) {
+                rfc2397.parse("data:text/plain;charset=cp866;foo=bar;answer=42,%e1%AB%ae%A2%ae", function (err, obj) {
+                    if (err)
+                        return done(err);
+                    expect(obj).to.deep.equal({
+                        mime: "text/plain",
+                        parameters: {
+                            charset: "cp866",
+                            foo: "bar",
+                            answer: "42",
+                        },
+                        data: Buffer.from([0xe1, 0xab, 0xae, 0xa2, 0xae]),
+                    });
+                    return done();
+                });
+            });
+        });
+        describe("when given a dataurl with base64 encoded text with mime specified", function () {
+            it("should parse it successfully", function (done) {
+                rfc2397.parse("data:text/plain;base64,SGVsbG8gV29ybGQ=", function (err, obj) {
+                    if (err)
+                        return done(err);
+                    expect(obj).to.deep.equal({
+                        mime: "text/plain",
+                        parameters: {
+                            charset: "US-ASCII",
+                        },
+                        data: Buffer.from("Hello World"),
+                    });
+                    return done();
+                });
+            });
+        });
+        describe("when given a dataurl with base64 encoded text with mime and charset specified", function () {
+            it("should parse it successfully", function (done) {
+                var data = "QXMtdHUgZOlq4CBmYWl0IGNlcyBy6nZlcyBO6W8sIHF1aSBz" +
+                    "ZW1ibGVudCBwbHVzIHZyYWlzIHF1ZSBsYSBy6WFsaXTpID8K";
+                rfc2397.parse("data:text/plain;charset=ISO-8859-1;base64," + data, function (err, obj) {
+                    if (err)
+                        return done(err);
+                    expect(obj).to.deep.equal({
+                        mime: "text/plain",
+                        parameters: {
+                            charset: "ISO-8859-1",
+                        },
+                        data: Buffer.from(data, "base64"),
+                    });
+                    return done();
+                });
+            });
+        });
+        describe("when given a minimal dataurl with base64 encoded text", function () {
+            it("should parse it successfully", function (done) {
+                rfc2397.parse("data:;base64,QSBicmllZiBub3Rl", function (err, obj) {
+                    if (err)
+                        return done(err);
+                    expect(obj).to.deep.equal({
+                        mime: "text/plain",
+                        parameters: {
+                            charset: "US-ASCII",
+                        },
+                        data: Buffer.from("A brief note"),
+                    });
+                    return done();
+                });
+            });
+        });
+        describe("when given a minimal dataurl with a base64 encoded image", function () {
+            it("should parse it successfully", function (done) {
+                var data = "R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
+                rfc2397.parse("data:image/gif;base64," + data, function (err, obj) {
+                    if (err)
+                        return done(err);
+                    expect(obj).to.deep.equal({
+                        mime: "image/gif",
+                        parameters: {},
+                        data: Buffer.from(data, "base64"),
+                    });
+                    return done();
+                });
+            });
+        });
+        describe("when given dataurl is invalid", function () {
+            it("should callback a 'malformed dataurl' error", function (done) {
+                rfc2397.parse("I am NOT a dataurl", function (err, dataurl) {
+                    expect(err).to.be.an.instanceof(Error);
+                    expect(err.message).to.equal("malformed dataurl");
+                    return done();
+                });
+            });
+        });
+        describe("when given a dataurl with bad data", function () {
+            it("should callback a 'malformed data' error", function (done) {
+                rfc2397.parse("data:,%fgabc", function (err, dataurl) {
+                    expect(err).to.be.an.instanceof(Error);
+                    expect(err.message).to.equal("malformed data");
+                    return done();
+                });
+            });
+        });
+    });
+    describe("compose", function () {
+        describe("when given RFC 2397 examples", function () {
+            it("should compose the 'brief note' example successfully", function (done) {
+                var obj = {
+                    data: Buffer.from("A brief note", "ascii")
+                };
+                rfc2397.compose(obj, function (err, dataurl) {
+                    if (err)
+                        return done(err);
+                    expect(dataurl).to.equal("data:,A%20brief%20note");
+                    return done();
+                });
+            });
+            it("should compose the 'charset parameter' example successfully", function (done) {
+                var obj =  {
+                    mime: "text/plain",
+                    parameters: {
+                        charset: "iso-8859-7",
+                    },
+                    data: Buffer.from("bed3be", "hex"),
+                };
+                rfc2397.compose(obj, function (err, dataurl) {
+                    if (err)
+                        return done(err);
+                    expect(dataurl).to.equal("data:text/plain;charset=iso-8859-7,%be%d3%be");
+                    return done();
+                });
+            });
+            it("should compose the 'Larry' example successfully", function (done) {
+                var data = "R0lGODdhMAAwAPAAAAAAAP///ywAAAAAMAAwAAAC8Iy" +
+                "Pqcvt3wCcDkiLc7C0qwyGHhSWpjQu5yqmCYsapyuvUUlvONmOZtfzgFz" +
+                    "ByTB10QgxOR0TqBQejhRNzOfkVJ+5YiUqrXF5Y5lKh/DeuNcP5yLWGsE" +
+                    "btLiOSpa/TPg7JpJHxyendzWTBfX0cxOnKPjgBzi4diinWGdkF8kjdfn" +
+                    "ycQZXZeYGejmJlZeGl9i2icVqaNVailT6F5iJ90m6mvuTS4OK05M0vDk" +
+                    "0Q4XUtwvKOzrcd3iq9uisF81M1OIcR7lEewwcLp7tuNNkM3uNna3F2JQ" +
+                    "Fo97Vriy/Xl4/f1cf5VWzXyym7PHhhx4dbgYKAAA7";
+                var larry = "data:image/gif;base64," + data;
+                var obj = {
+                    mime: 'image/gif',
+                    parameters: {},
+                    data: Buffer.from(data, "base64"),
+                };
+                rfc2397.compose(obj, { base64: true }, function (err, dataurl) {
+                    if (err)
+                        return done(err);
+                    expect(dataurl).to.equal(larry);
+                    return done();
+                });
+            });
+            it("should compose the 'application/vnd-xxx-query' example successfully", function (done) {
+                var obj = {
+                    mime: 'application/vnd-xxx-query',
+                    parameters: {},
+                    data: Buffer.from("select_vcount,fcol_from_fieldtable/local", "ascii"),
+                };
+                var data = "select_vcount%2cfcol_from_fieldtable%2flocal";
+                var vnd = "data:application/vnd-xxx-query," + data;
+                rfc2397.compose(obj, function (err, dataurl) {
+                    if (err)
+                        return done(err);
+                    expect(dataurl).to.equal(vnd);
+                    return done();
+                });
+            });
+        });
+        describe("when given an object with several parameters", function () {
+            it("should compose it successfully", function (done) {
+                var obj = {
+                    mime: "text/plain",
+                    parameters: {
+                        charset: "cp866",
+                        foo: "bar",
+                        answer: "42",
+                    },
+                    data: Buffer.from([0xe1, 0xab, 0xae, 0xa2, 0xae]),
+                };
+                rfc2397.compose(obj, function (err, dataurl) {
+                    if (err)
+                        return done(err);
+                    expect(dataurl).to.equal("data:text/plain;charset=cp866;foo=bar;answer=42,%e1%ab%ae%a2%ae");
+                    return done();
+                });
+            });
+        });
+        describe("when given an object with text to encode to base64", function () {
+            it("should compose it successfully", function (done) {
+                var obj = {
+                    data: Buffer.from("A brief note"),
+                };
+                rfc2397.compose(obj, { base64: true }, function (err, dataurl) {
+                    if (err)
+                        return done(err);
+                    expect(dataurl).to.equal("data:;base64,QSBicmllZiBub3Rl");
+                    return done();
+                });
+            });
+        });
+        describe("when given an object with text to encode to base64 and mime is specified", function () {
+            it("should compose it successfully", function (done) {
+                var obj = {
+                    mime: "text/plain",
+                    data: Buffer.from("Hello World"),
+                };
+                rfc2397.compose(obj, { base64: true }, function (err, dataurl) {
+                    if (err)
+                        return done(err);
+                    expect(dataurl).to.equal("data:text/plain;base64,SGVsbG8gV29ybGQ=");
+                    return done();
+                });
+            });
+        });
+        describe("when given an object with text to encode to base64 and mime and charset are specified", function () {
+            it("should compose it successfully", function (done) {
+                var data = "QXMtdHUgZOlq4CBmYWl0IGNlcyBy6nZlcyBO6W8sIHF1aSBz" +
+                    "ZW1ibGVudCBwbHVzIHZyYWlzIHF1ZSBsYSBy6WFsaXTpID8K";
+                var obj = {
+                    mime: "text/plain",
+                    parameters: {
+                        charset: "ISO-8859-1",
+                    },
+                    data: Buffer.from(data, "base64"),
+                };
+                rfc2397.compose(obj, { base64: true }, function (err, dataurl) {
+                    if (err)
+                        return done(err);
+                    expect(dataurl).to.equal("data:text/plain;charset=ISO-8859-1;base64," + data);
+                    return done();
+                });
+            });
+        });
+        describe("when given an object with a base64 encoded image", function () {
+            it("should compose it successfully", function (done) {
+                var data = "R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
+                var obj = {
+                    mime: "image/gif",
+                    data: Buffer.from(data, "base64"),
+                };
+                rfc2397.compose(obj, { base64: true }, function (err, dataurl) {
+                    if (err)
+                        return done(err);
+                    expect(dataurl).to.equal("data:image/gif;base64," + data);
+                    return done();
+                });
+            });
+        });
+        describe("when the given charset encoding does not exist", function () {
+            it("should callback an 'unsupported charset' error", function (done) {
+                var obj = {
+                    mime: "text/plain",
+                    parameters: {
+                        charset: "klingon",
+                    },
+                    data: Buffer.from("Heghlu'meH QaQ jajvam!"),
+                };
+                rfc2397.compose(obj, function (err, dataurl) {
+                    expect(err).to.be.an.instanceof(Error);
+                    expect(err.message).to.equal("unsupported charset (klingon)");
+                    return done();
+                });
+            });
+        });
+        describe("when the given data is not a Buffer", function () {
+            it("should callback an 'unexpected type for obj.data' error", function (done) {
+                var obj = {
+                    data: new Date(),
+                };
+                rfc2397.compose(obj, function (err, dataurl) {
+                    expect(err).to.be.an.instanceof(Error);
+                    expect(err.message).to.equal("unexpected type for obj.data (did you provide a Buffer?)");
+                    return done();
+                });
+            });
+        });
+    });
+});
